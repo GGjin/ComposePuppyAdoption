74e35ed976e74db830158458c3cb94bb4d9b786a
Jinyu.Guo3 <gg.jin.yu@gmail.com>
2021-03-02 09:16:40 +0800

    lint检查

--- app/src/main/java/com/example/androiddevchallenge/PuppyDetailActivity.kt
+++ app/src/main/java/com/example/androiddevchallenge/PuppyDetailActivity.kt
@@ -4,8 +4,12 @@ import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Card
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
